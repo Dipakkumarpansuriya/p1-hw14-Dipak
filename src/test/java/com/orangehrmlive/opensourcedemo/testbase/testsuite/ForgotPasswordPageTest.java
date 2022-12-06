7fcf9e8e87789e06a6fe82c886ebeb5d897165f7
primetraineesix <primetraineesix@gmail.com>
2022-12-06 21:42:04 +0000

    Dipak

---
+++ src/test/java/com/orangehrmlive/opensourcedemo/testbase/testsuite/ForgotPasswordPageTest.java
@@ -0,0 +1,18 @@
+package com.orangehrmlive.opensourcedemo.testbase.testsuite;
+
+import com.orangehrmlive.opensourcedemo.pages.ForgotPasswordPage;
+import com.orangehrmlive.opensourcedemo.testbase.testbase.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ForgotPasswordPageTest extends BaseTest {
+    ForgotPasswordPage forgotPasswordPage = new ForgotPasswordPage();
+
+    @Test
+    public void userShouldNavigateToForgotPasswordPageSuccessfully(){
+        forgotPasswordPage.clickOnForgotPasswordLink();
+        String expectedMessage  = "Reset Password";
+        Assert.assertEquals(forgotPasswordPage.getResetPasswordMessage(),expectedMessage,"Not matching message");
+    }
+
+}
